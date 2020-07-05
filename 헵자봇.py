65689b32fde76044d804e66176f715b8bf1f0f54
sobe15 <67471844+sobe15@users.noreply.github.com>
2020-07-05 12:59:49 +0900

    Add files via upload

---
+++ 헵자봇.py
@@ -0,0 +1,35 @@
+import discord
+
+client = discord.Client()
+
+@client.event 
+async def on_ready():
+    print('We Have Iogged in as {0.user}'.format(client))
+
+def method_name():
+    return 'Hello!'
+
+async def method_name(message):
+    await message.channel.send(method_name())
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$강욱아'):
+        await message.channel.send('왜! 뭐! 왜부르는데!!')
+
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$야'):
+        await message.channel.send('왜 무머머ㅓ머무머뭠뭐뭐뭐뭐뭐뭐뭐!!!!!!!')
+
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$송'):
+        await message.channel.send('강욱')
+
+client.run('NzI1Mjc1MTQxMTUxNTIyODg4.XwFGhw.SWvMz3_5xnn-BVIpSZmkxKEqXPE')
